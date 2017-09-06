71b5be20e6947205a3be34b1d65d5972f5dc5587
Elena Esina <hellemess@yandex.ru>
2017-09-06 11:25:55 +0300

    перенесла массив с лотами в отдельный сценарий, сформировала ссылки для каждого из лотов и настроила отображение каждого лота по соответствующей ссылке, настроила возвращение кода ответа 404 при передаче несуществующего идентификатора, убрала html-код из lot.php

--- functions.php
+++ functions.php
@@ -1,5 +1,60 @@
 <?php
 
+function convert_ts($ts) {
+    $now = strtotime('now');
+    $time_passed = $now - $ts;
+
+    if ($time_passed < SECONDS_IN_MINUTE) {
+        $time_passed = 'Только что';
+    } elseif ($time_passed < SECONDS_IN_HOUR) {
+        $time_passed = floor($time_passed / SECONDS_IN_MINUTE);
+
+        if ($time_passed == 1) {
+            $time_passed = 'Минуту назад';
+        } elseif ($time_passed > 10 && $time_passed < 20) {
+            $time_passed = $time_passed . ' минут назад';
+        } else {
+            switch ($time_passed % 10) {
+                case 1:
+                    $time_passed = $time_passed . ' минуту назад';
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    $time_passed = $time_passed . ' минуты назад';
+                    break;
+                default:
+                    $time_passed = $time_passed . ' минут назад';
+            }
+        }
+    } elseif ($time_passed < SECONDS_IN_DAY) {
+        $time_passed = floor($time_passed / SECONDS_IN_HOUR);
+
+        if ($time_passed == 1) {
+            $time_passed = 'Час назад';
+        } elseif ($time_passed > 10 && $time_passed < 20) {
+            $time_passed = $time_passed . ' часов назад';
+        } else {
+            switch ($time_passed % 10) {
+                case 1:
+                    $time_passed = $time_passed . ' час назад';
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    $time_passed = $time_passed . ' часа назад';
+                    break;
+                default:
+                    $time_passed = $time_passed . ' часов назад';
+            }
+        }
+    } else {
+        $time_passed = date('d.m.y в H:i', $ts);
+    }
+
+    return $time_passed;
+}
+
 function get_html_code($template, $data) {
     extract($data);
 
--- templates/index.php
+++ templates/index.php
@@ -33,18 +33,18 @@
         </select>
     </div>
     <ul class="lots__list">
-    <?php foreach ($lots as $lot): ?>
+    <?php foreach ($lots as $id => $lot): ?>
         <li class="lots__item lot">
             <div class="lot__image">
                 <img src="<?=$lot['picture']; ?>" width="350" height="260" alt="<?=htmlspecialchars($lot['alt']); ?>">
             </div>
             <div class="lot__info">
                 <span class="lot__category"><?=$lot['category']; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="lot.php"><?=htmlspecialchars($lot['title']); ?></a></h3>
+                <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?=$id; ?>"><?=htmlspecialchars($lot['title']); ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
+                        <span class="lot__cost"><?=$lot['starting_price']; ?><b class="rub">р</b></span>
                     </div>
                     <div class="lot__timer timer">
                         <?=$lot_time_remaining; ?>
