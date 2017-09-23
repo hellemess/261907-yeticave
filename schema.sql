bb57db989f8a28fece92a20ae2606036c70849e1
Elena Esina <hellemess@yandex.ru>
2017-09-23 22:28:41 +0300

    изменила тип полей, чтобы создавалась связь между таблицами

--- schema.sql
+++ schema.sql
@@ -9,6 +9,16 @@ CREATE TABLE categories (
   title CHAR(15) NOT NULL
 );
 
+CREATE TABLE users (
+  id                INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  email             CHAR(100) NOT NULL,
+  registration_date DATETIME NOT NULL,
+  name              CHAR NOT NULL,
+  password          CHAR(60) NOT NULL,
+  avatar            CHAR(20),
+  contacts          TEXT
+);
+
 CREATE TABLE lots (
   id              INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   creation_date   DATETIME NOT NULL,
@@ -19,33 +29,22 @@ CREATE TABLE lots (
   expiration_date DATE NOT NULL,
   step            INT NOT NULL,
   likes           INT,
-  seller          CHAR(100) NOT NULL,
-  winner          CHAR(100),
-  category        CHAR(15) NOT NULL,
-  FOREIGN KEY (seller) REFERENCES users (id),
-  FOREIGN KEY (winner) REFERENCES users (id),
-  FOREIGN KEY (category) REFERENCES categories (id)
-
+  seller          INT(10) NOT NULL,
+  winner          INT(10),
+  category        INT(10) NOT NULL,
+  FOREIGN KEY (seller) REFERENCES users(id),
+  FOREIGN KEY (winner) REFERENCES users(id),
+  FOREIGN KEY (category) REFERENCES categories(id)
 );
 
 CREATE TABLE bets (
   id           INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   betting_date DATETIME NOT NULL,
   cost         INT NOT NULL,
-  buyer        CHAR(100) NOT NULL,
-  lot          INT NOT NULL,
-  FOREIGN KEY (buyer) REFERENCES users (id),
-  FOREIGN KEY (lot) REFERENCES lots (id)
-);
-
-CREATE TABLE users (
-  id                INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  email             CHAR(100) NOT NULL,
-  registration_date DATETIME NOT NULL,
-  name              CHAR NOT NULL,
-  password          CHAR(60) NOT NULL,
-  avatar            CHAR(20),
-  contacts          TEXT
+  buyer        INT(10) NOT NULL,
+  lot          INT(10) NOT NULL,
+  FOREIGN KEY (buyer) REFERENCES users(id),
+  FOREIGN KEY (lot) REFERENCES lots(id)
 );
 
 CREATE UNIQUE INDEX c_title ON categories(title);
