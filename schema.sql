5bf451fd697914155d4a6fc13199eb4fb9aab084
Elena Esina <hellemess@yandex.ru>
2017-09-24 17:25:37 +0300

    заполнила таблицы, написала запросы на выборку данных

--- schema.sql
+++ schema.sql
@@ -5,50 +5,50 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE categories (
-  id    INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  title CHAR(15) NOT NULL
+  id    INT (10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title CHAR (15) NOT NULL
 );
 
 CREATE TABLE users (
-  id                INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  email             CHAR(100) NOT NULL,
-  registration_date DATETIME NOT NULL,
-  name              CHAR NOT NULL,
-  password          CHAR(60) NOT NULL,
-  avatar            CHAR(20),
+  id                INT (10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  email             CHAR (100) NOT NULL,
+  registration_date DATETIME,
+  name              CHAR (100) NOT NULL,
+  password          CHAR (60) NOT NULL,
+  avatar            CHAR (20),
   contacts          TEXT
 );
 
 CREATE TABLE lots (
-  id              INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id              INT (10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   creation_date   DATETIME NOT NULL,
-  title           CHAR(100) NOT NULL,
+  title           CHAR (100) NOT NULL,
   description     TEXT NOT NULL,
-  picture         CHAR(20) NOT NULL,
+  picture         CHAR (20) NOT NULL,
   starting_price  INT NOT NULL,
   expiration_date DATE NOT NULL,
   step            INT NOT NULL,
   likes           INT,
-  seller          INT(10) NOT NULL,
-  winner          INT(10),
-  category        INT(10) NOT NULL,
-  FOREIGN KEY (seller) REFERENCES users(id),
-  FOREIGN KEY (winner) REFERENCES users(id),
-  FOREIGN KEY (category) REFERENCES categories(id)
+  seller          INT (10) NOT NULL,
+  winner          INT (10),
+  category        INT (10) NOT NULL,
+  FOREIGN KEY (seller) REFERENCES users (id),
+  FOREIGN KEY (winner) REFERENCES users (id),
+  FOREIGN KEY (category) REFERENCES categories (id)
 );
 
 CREATE TABLE bets (
-  id           INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id           INT (10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   betting_date DATETIME NOT NULL,
   cost         INT NOT NULL,
-  buyer        INT(10) NOT NULL,
-  lot          INT(10) NOT NULL,
-  FOREIGN KEY (buyer) REFERENCES users(id),
-  FOREIGN KEY (lot) REFERENCES lots(id)
+  buyer        INT (10) NOT NULL,
+  lot          INT (10) NOT NULL,
+  FOREIGN KEY (buyer) REFERENCES users (id),
+  FOREIGN KEY (lot) REFERENCES lots (id)
 );
 
-CREATE UNIQUE INDEX c_title ON categories(title);
+CREATE UNIQUE INDEX c_title ON categories (title);
 
-CREATE UNIQUE INDEX u_email ON users(email);
+CREATE UNIQUE INDEX u_email ON users (email);
 
-CREATE INDEX l_title ON lots(title);
+CREATE INDEX l_title ON lots (title);
